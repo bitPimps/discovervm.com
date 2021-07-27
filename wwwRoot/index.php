2090bc7197aa0e100fc55869b3d4ed266b805bb7
bitPimps <bitpimps@gmail.com>
2021-07-27 09:26:18 -0500

    Update css link

--- wwwRoot/index.php
+++ wwwRoot/index.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link href="https://www.barralinstitute.com/css/main.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://www.barralinstitute.com/css/main.min.css">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
